261648f0b2483f329d41404ce590c4b033535072
Mihaela Sighireanu <sighirea@irif.fr>
2019-04-07 15:06:10 +0200

    presentation at TOOLympics

---
+++ doc/slides/publish.el
@@ -0,0 +1,13 @@
+;;
+;;
+
+
+(setf user-full-name "Mihaela Sighireanu")
+(setf user-mail-address "sl-comp@googlegroups.com")
+
+;; activate debugging
+(setq debug-on-error t
+      debug-on-signal nil
+      debug-on-quit nil)
+
+(org-beamer-export-to-latex)
